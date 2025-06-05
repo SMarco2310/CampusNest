266cef9348d702e494ad466573074ce91e871f1d
Marc-Etienne sossou <marc.sossou@ashesi.edu.gh>
2025-06-05 09:10:32 +0000

    Load Firebase service account key from environment variable in main method

--- Back-End/CampusNest/src/main/java/com/example/campusnest/CampusNestApplication.java
+++ Back-End/CampusNest/src/main/java/com/example/campusnest/CampusNestApplication.java
@@ -3,10 +3,16 @@ package com.example.campusnest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @SpringBootApplication
 public class CampusNestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        FileInputStream serviceAccount = new FileInputStream(System.getenv("firebase-service-account.json"));
+
         SpringApplication.run(CampusNestApplication.class, args);
     }
 
